cbb3b1d52e1539882eb94086dc46325a50bafcab
satellite <bluesatellite777@gmail.com>
2023-02-12 08:10:37 +1100

    Feature/backend/REST_API v1.0

---
+++ backend/src/test/java/satellite/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package satellite.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
