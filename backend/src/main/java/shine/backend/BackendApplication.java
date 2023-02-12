f794cb8c0d3d95560f606d72f2247ea1f7beaf8a
satellite <bluesatellite777@gmail.com>
2023-02-12 17:08:49 +1100

    Feature/backend/REST_API V1.1

--- backend/src/main/java/shine/backend/BackendApplication.java
+++ backend/src/main/java/shine/backend/BackendApplication.java
@@ -1,4 +1,4 @@
-package satellite.backend;
+package shine.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- backend/src/test/java/shine/backend/BackendApplicationTests.java
+++ backend/src/test/java/shine/backend/BackendApplicationTests.java
@@ -1,4 +1,4 @@
-package satellite.backend;
+package shine.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
