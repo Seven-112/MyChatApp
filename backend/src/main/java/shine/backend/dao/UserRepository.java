31df69ec3b8045bcccb45b2532cccf3c8b368550
satellite <bluesatellite777@gmail.com>
2023-02-14 18:27:09 +1100

    Docker

--- backend/src/main/java/shine/backend/dao/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package shine.backend.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import shine.backend.entity.User;
-
-public interface UserRepository extends JpaRepository<User, Integer> {
-
-}
--- backend/src/test/java/com/docker/backend/BackendApplicationTests.java
+++ backend/src/test/java/com/docker/backend/BackendApplicationTests.java
@@ -1,4 +1,4 @@
-package shine.backend;
+package com.docker.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
